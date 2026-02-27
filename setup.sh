db8487974af0279085ae5633e277f39406c8c28e
ayo-ore <42537519+ayo-ore@users.noreply.github.com>
2026-02-27 20:16:40 +0100

    Clean `setup.sh`
    Removed module loading commands and adjusted threading environment variables.

--- setup.sh
+++ setup.sh
@@ -3,10 +3,6 @@
 # project directory
 export AUSSIE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# modules
-ml purge
-ml cuda/12.8 cudnn/11.6 anaconda/3.0
-
 # conda
 eval "$(conda shell.bash hook)"
 conda activate aussie
@@ -19,6 +15,3 @@ export HYDRA_FULL_ERROR=1
 
 # resolve multithreading conflict
 export MKL_THREADING_LAYER=GNU
-
-export OMP_NUM_THREADS=32
-export NUMEXPR_MAX_THREADS=32
